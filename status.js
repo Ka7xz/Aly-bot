5fe091ff8308ee4bcd06d70dd42a59eb88fbbf25
Ka7xz <hamdraza999@gmail.com>
2026-09-09 08:10:41 +0530

    Add setAlyStatus function to update client presence

--- status.js
+++ status.js
@@ -1,1 +1,16 @@
+function setAlyStatus(client) {
+  client.user.setPresence({
+    status: "online",
+    activities: [
+      {
+        name: "Favorite collectible?",
+        type: 4,
+        state: "Favorite collectible?"
+      }
+    ]
+  });
+}
 
+module.exports = {
+  setAlyStatus
+};
