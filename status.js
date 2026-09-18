9d753097b81f9e75e1b55fc997e69b5114f7ca54
Ka7xz <hamdraza999@gmail.com>
2026-09-19 02:24:45 +0530

    Update status.js

--- status.js
+++ status.js
@@ -1,36 +1,13 @@
 function setAlyStatus(client) {
-  const statuses = [
-    "Chatting with the server",
-    "Hanging out on Discord",
-    "Talking with everyone",
-    "Just being Aly"
-  ];
-
-  let index = 0;
-
-  const updateStatus = () => {
-    if (!client.user) return;
-
-    client.user.setPresence({
-      activities: [
-        {
-          name: statuses[index],
-          type: 0
-        }
-      ],
-      status: "online"
-    });
-
-    index =
-      (index + 1) % statuses.length;
-  };
-
-  updateStatus();
-
-  setInterval(
-    updateStatus,
-    60000
-  );
+  client.user.setPresence({
+    activities: [
+      {
+        name: "Kinda Busy In Chat",
+        type: 0
+      }
+    ],
+    status: "online"
+  });
 }
 
 module.exports = {
