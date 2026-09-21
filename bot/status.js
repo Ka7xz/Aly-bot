65e657449bcb19d1580a8c47a13ad01017ca5d81
Ka7x <hamdraza999@gmail.com>
2026-09-22 04:15:25 +0530

    Update status.js

--- bot/status.js
+++ bot/status.js
@@ -1,28 +1,14 @@
 const { ActivityType } = require("discord.js");
 
 module.exports = (client) => {
-  const updateStatus = () => {
-    const members = client.guilds.cache.reduce(
-      (total, guild) => total + guild.memberCount,
-      0
-    );
-
-    client.user.setPresence({
-      status: "online",
-      activities: [
-        {
-          name: "custom",
-          type: ActivityType.Custom,
-          state: "FEEL FREE TO TALK"
-        },
-        {
-          name: `${members} Members`,
-          type: ActivityType.Listening
-        }
-      ]
-    });
-  };
-
-  updateStatus();
-  setInterval(updateStatus, 5 * 60 * 1000);
+  client.user.setPresence({
+    status: "online",
+    activities: [
+      {
+        name: "custom",
+        type: ActivityType.Custom,
+        state: "FEEL FREE TO TALK"
+      }
+    ]
+  });
 };
